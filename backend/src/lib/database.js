4aa68f27a1d5891f6f800f1fadafb14e53fb847d
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-05-10 01:59:52 +0530

    feat: setup database, Clerk auth, Inngest, and Stream integrations

---
+++ backend/src/lib/database.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import { ENV } from './env.js';
+
+export const connectDB = async () => {
+    if(!ENV.DB_URL){
+        throw new Error("DB_URL is not defined in environment variables");
+    }
+   const conn =  await mongoose.connect(ENV.DB_URL)
+   return conn;
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/database.js';
 
 const app = express();
 
@@ -7,6 +8,13 @@ app.get("/health",(req,res) => {
     res.status(200).json({msg: "OK"});
 })
 
-app.listen(ENV.PORT,() => {
-    console.log("\nServer is running on port " + ENV.PORT);
-})
+connectDB()
+  .then((conn) => {
+    console.log("\n✅ Connected to MongoDB : ", conn.connection.host, conn.connection.name);
+    app.listen(ENV.PORT,() => {
+        console.log("Server is running on port " + ENV.PORT);
+    })
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB ❌", err);
+  });
