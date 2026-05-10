fdbb7f2f9c6f910b4e681bb8b4aae2ded8ee31a1
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-05-11 01:17:34 +0530

    feat: implement Clerk user sync and deletion with Inngest & webHook, added user schema model

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,37 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./database.js";
+import { User } from "../models/User.js";
+
+export const inngest = new Inngest({ id: "InterviewPro" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user", event: "clerk.user.created" },
+  async ({ event }) => {
+    // get user details from event data and create user in db
+    const { id, email_addresses, first_name, last_name, profile_image_url } =
+      event.data;
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      profileImg: profile_image_url || "",
+    };
+    const user = await User.create(newUser);
+    console.log("Created user : ", user);
+    //todo : do something else
+  },
+);
+
+// delete user when user is deleted in clerk (assuming clerk.user.deleted event is triggered on user deletion in clerk)
+const deleteUser = inngest.createFunction(
+  { id: "delete-user", event: "clerk.user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    const user = await User.findOneAndDelete({ clerkId: id });
+    console.log("Deleted user : ", user);
+
+    //todo : do something else
+  },
+);
+
+export const InngestFunctions = [syncUser, deleteUser];
---
+++ backend/src/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImg: {
+      type: String,
+      default: "",
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
