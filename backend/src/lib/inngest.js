c72ad2f83ca85b83788e9d6998d91cd3adecf518
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-05-11 02:11:23 +0530

    fix: correct Clerk Inngest event

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,15 +1,15 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./database.js";
 import { User } from "../models/User.js";
 
 export const inngest = new Inngest({ id: "InterviewPro" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user", event:"clerk/user.created" },
+  { 
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }] 
+  },
   async ({ event }) => {
-    // get user details from event data and create user in db
-    const { id, email_addresses, first_name, last_name, profile_image_url } =
-      event.data;
+    const { id, email_addresses, first_name, last_name, profile_image_url } = event.data;
     const newUser = {
       clerkId: id,
       email: email_addresses[0].email_address,
@@ -17,21 +17,20 @@ const syncUser = inngest.createFunction(
       profileImg: profile_image_url || "",
     };
     const user = await User.create(newUser);
-    console.log("Created user : ", user);
-    //todo : do something else
-  },
+    console.log("Created user:", user);
+  }
 );
 
-// delete user when user is deleted in clerk (assuming clerk.user.deleted event is triggered on user deletion in clerk)
 const deleteUser = inngest.createFunction(
-  { id: "delete-user", event: "clerk/user.deleted" },
+  { 
+    id: "delete-user",
+    triggers: [{ event: "clerk/user.deleted" }] 
+  },
   async ({ event }) => {
     const { id } = event.data;
     const user = await User.findOneAndDelete({ clerkId: id });
-    console.log("Deleted user : ", user);
-
-    //todo : do something else
-  },
+    console.log("Deleted user:", user);
+  }
 );
 
 export const InngestFunctions = [syncUser, deleteUser];
