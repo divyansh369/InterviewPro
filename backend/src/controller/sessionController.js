b4575524d7a96172bc97783791402c8249232926
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-05-14 22:13:49 +0530

    Check: refactoring the sessionController

--- backend/src/controller/sessionController.js
+++ backend/src/controller/sessionController.js
@@ -25,29 +25,44 @@ export const createSession = async (req, res) => {
       callId,
     });
 
-    // create a stream video call
-    await streamClient.video.call("default", callId).getOrCreate({
-      data: {
+    let videoCallCreated = false;
+    try {
+      // create a stream video call
+      await streamClient.video.call("default", callId).getOrCreate({
+        data: {
+          created_by_id: clerkId,
+          custom: { problem, difficulty, sessionId: session._id.toString() },
+        },
+      });
+
+      // set to true if video call is created
+      videoCallCreated = true;
+
+      // create a stream chat messaging
+      const chatChannel = chatClient.channel("messaging", callId, {
+        name: `Session Chat - ${problem}`,
+        members: [clerkId], // add the host to the chat channel which will be 1 when created
         created_by_id: clerkId,
-        custom: { problem, difficulty, sessionId: session._id.toString() },
-      },
-    });
+      });
 
-    // chat messaging
-    const chatChannel = chatClient.channel("messaging", callId, {
-      name: `Session Chat - ${problem}`,
-      members: [clerkId], // add the host to the chat channel which will be 1 when created
-      created_by_id: clerkId,
-    });
+      await chatChannel.create();
+    } catch (error) {
+      // delete the session if video call is not created
+      await Session.findByIdAndDelete(session._id);
 
-    await chatChannel.create();
+      if (videoCallCreated) {
+        await streamClient.video
+          .call("default", callId)
+          .delete({ hard: true })
+          .catch(console.error);
+      }
+      throw error;
+    }
 
-    res
-      .status(201)
-      .json({ msg: "Session created successfully", session: session });
+    res.status(201).json({ msg: "Session created successfully", session });
+
   } catch (error) {
     console.error("Error creating session:", error);
-
     res.status(500).json({
       msg: "Internal server error",
       error: error.message,
@@ -132,7 +147,6 @@ export const getSessionById = async (req, res) => {
 // *** for joining session from the other user (not host) perspective ***
 export const joinSession = async (req, res) => {
   try {
-
     const { sessionId } = req.params;
 
     const userId = req.user._id;
@@ -140,76 +154,79 @@ export const joinSession = async (req, res) => {
 
     const sessionToJoin = await Session.findById(sessionId);
 
-    if(!sessionToJoin){
-        return res.status(404).json({ msg: "Session not found" });  
+    if (!sessionToJoin) {
+      return res.status(404).json({ msg: "Session not found" });
     }
 
     // check if the session is full (for example max 2 participants for now)
-    if(sessionToJoin.participants.length >=2){
-        return res.status(400).json({ msg: "Session is full" });
+    if (sessionToJoin.participants.length >= 2) {
+      return res.status(409).json({ msg: "Session is full" });
     }
 
     // another check for active session
-    if(sessionToJoin.status !== "active"){
-        return res.status(400).json({ msg: "Session is not active" });
+    if (sessionToJoin.status !== "active") {
+      return res.status(400).json({ msg: "Session is not active" });
+    }
+
+    if(sessionToJoin.host.toString() === userId.toString()){
+      return res.status(400).json({ msg: "Host cannot join their session as a participant"});
     }
 
     // add the user to the session participants
-
     sessionToJoin.participants = userId;
     await sessionToJoin.save();
 
     // add the user to chat
-    const chatChannel = chatClient.channel("messaging", callId);
+    const chatChannel = chatClient.channel("messaging", sessionToJoin.callId);
     await chatChannel.addMembers([clerkId]);
 
-    res.status(200).json({ msg: "Joined session successfully", session: sessionToJoin });
-
+    res
+      .status(200)
+      .json({ msg: "Joined session successfully", session: sessionToJoin });
   } catch (error) {
     console.error("Error :", error);
     res.status(500).json({
       msg: "Internal server error",
       error: error.message,
     });
-
   }
 };
 
 export const endSession = async (req, res) => {
   try {
-
-    const {sessionId} = req.params;
+    const { sessionId } = req.params;
     const userId = req.user._id;
-    
+
     const sessionToEnd = await Session.findById(sessionId);
 
-    if(!sessionToEnd){
-        return res.status(404).json({ msg: "Session not found" });  
+    if (!sessionToEnd) {
+      return res.status(404).json({ msg: "Session not found" });
     }
 
     // check if user is the host of the session only host can end the session
-    if(sessionToEnd.host.toString() !== userId.toString()){
-        return res.status(403).json({ msg: "Only host can end the session" });
+    if (sessionToEnd.host.toString() !== userId.toString()) {
+      return res.status(403).json({ msg: "Only host can end the session" });
     }
 
     // if session is already completed
-    if(sessionToEnd.status === "completed"){
-        return res.status(400).json({ msg: "Session is already completed" });
+    if (sessionToEnd.status === "completed") {
+      return res.status(400).json({ msg: "Session is already completed" });
     }
-
-    sessionToEnd.status = "completed";
-    await sessionToEnd.save();
 
     // end the stream video call
     const call = streamClient.video.call("default", sessionToEnd.callId);
-    await call.delete({hard:true});
+    await call.delete({ hard: true });
 
     // end the stream chat channel
     const chatChannel = chatClient.channel("messaging", sessionToEnd.callId);
     await chatChannel.delete();
 
-    res.status(200).json({ msg: "Session ended successfully", session: sessionToEnd });
+    sessionToEnd.status = "completed";
+    await sessionToEnd.save();
 
+    res
+      .status(200)
+      .json({ msg: "Session ended successfully", session: sessionToEnd });
   } catch (error) {
     console.error("Error :", error);
     res.status(500).json({
